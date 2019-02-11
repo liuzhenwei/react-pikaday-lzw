5d83ed07b55e2f9c34cab8b16139ad3c52310d6c
Will <will.liu@livenaked.com>
2019-02-11 14:21:32 +0800

    1.0.5

--- Pikaday.js
+++ Pikaday.js
@@ -7,34 +7,21 @@ var PropTypes = require('prop-types');
 
 var Pikaday = require('pikaday');
 
-var ReactPikaday = React.createClass({
-	displayName: 'ReactPikaday',
+class ReactPikaday extends React.Component{
+	constructor(props) {
+		super(props);
 
-	propTypes: {
-		value: PropTypes.instanceOf(Date),
-		onChange: PropTypes.func,
-		initialOptions: PropTypes.object,
+		this.displayName = 'ReactPikaday';
+	}
 
-		valueLink: PropTypes.shape({
-			value: PropTypes.instanceOf(Date),
-			requestChange: PropTypes.func.isRequired
-		})
-	},
-
-	getDefaultProps: function getDefaultProps() {
-		return {
-			initialOptions: {}
-		};
-	},
-
-	getValueLink: function getValueLink(props) {
+	getValueLink(props) {
 		return props.valueLink || {
 			value: props.value,
 			requestChange: props.onChange
 		};
-	},
+	}
 
-	setDateIfChanged: function setDateIfChanged(newDate, prevDate) {
+	setDateIfChanged(newDate, prevDate) {
 		var newTime = newDate ? newDate.getTime() : null;
 		var prevTime = prevDate ? prevDate.getTime() : null;
 
@@ -45,9 +32,9 @@ var ReactPikaday = React.createClass({
 			}
 			this.picker.setDate(newDate, true); // 2nd param = don't call onSelect
 		}
-	},
+	}
 
-	componentDidMount: function componentDidMount() {
+	componentDidMount() {
 		var el = this.refs.pikaday;
 
 		this.picker = new Pikaday(objectAssign({
@@ -56,19 +43,34 @@ var ReactPikaday = React.createClass({
 		}, this.props.initialOptions));
 
 		this.setDateIfChanged(this.getValueLink(this.props).value);
-	},
+	}
 
-	componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+	componentWillReceiveProps(nextProps) {
 		var newDate = this.getValueLink(nextProps).value;
 		var lastDate = this.getValueLink(this.props).value;
 
 		this.setDateIfChanged(newDate, lastDate);
-	},
+	}
 
-	render: function render() {
+	render() {
 		return React.createElement('input', { type: 'text', ref: 'pikaday', className: this.props.className, name: this.props.name,
 			placeholder: this.props.placeholder, disabled: this.props.disabled, readOnly: this.props.readOnly, autoComplete: this.props.autoComplete });
 	}
-});
+};
+
+ReactPikaday.propTypes = {
+	value: PropTypes.instanceOf(Date),
+	onChange: PropTypes.func,
+	initialOptions: PropTypes.object,
+
+	valueLink: PropTypes.shape({
+		value: PropTypes.instanceOf(Date),
+		requestChange: PropTypes.func.isRequired
+	})
+};
+
+ReactPikaday.defaultProps = {
+	initialOptions: {},
+};
 
 module.exports = ReactPikaday;
